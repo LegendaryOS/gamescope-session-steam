ee69e86d858319af219816e5f15d3c6d00cc9e9e
Michal92299 <michal.99967@gmail.com>
2025-07-04 22:25:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+#Sudo mode
+sudo su
+
+#gamescope install
+USER_HOME="/home/$(whoami)"
+SOURCE="/tmp/gamescope-session-steam/gamescope"
+DEST="$USER_HOME/.hackeros"
+mv "$SOURCE"/* "$DEST"/
+
